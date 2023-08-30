a0fd36075442c6724820028bae9ff68b51713394
dextern31 <140563233+dextern31@users.noreply.github.com>
2023-08-30 15:10:45 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-/* eslint-disable */
+/* eslint-disable 
 import "bootstrap";
 import "./style.css";
 
@@ -8,4 +8,20 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
-};
+};*/
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+let extension = [".com", ".net", ".gov"];
+
+
+for (let i = 0; i < pronoun.length; i++) {
+  for (let j = 0; j < adj.length; j++) {
+    for (let k = 0; k < noun.length; k++) {
+      for (let l = 0; l<extension.length; l++) {
+        let domain = `${pronoun[i]}${adj[j]}${noun[k]}${extension[l]}`;
+        console.log(domain);
+      }      
+    }
+  }
+}
